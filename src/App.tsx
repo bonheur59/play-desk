3166b02ebc7cb63367d908b21fd53ea1a4dccb08
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2024-01-26 16:35:57 +0900

    desc : Add initial project configuration
    vite, tailwind, shadcn-ui 설치 및 설정 추가

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Button>Click me</Button>
+      </div>
+      <div className="text-3xl font-bold underline">it goes Work!!</div>
+    </div>
+  );
+}
+
+export default App;
