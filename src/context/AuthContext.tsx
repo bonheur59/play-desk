031175809021d01f9103462b57f235294803a845
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2024-01-31 09:13:34 +0900

    feat : 로그인 된 계정 Context에서 전역 관리

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,61 @@
+import { ReactNode, createContext, useState, useEffect } from "react";
+import { User, getAuth, onAuthStateChanged } from "firebase/auth";
+import { getUserDataFromFirestore } from "../firebaseUtil";
+import { app } from "@/firebaseApp";
+
+interface AuthProps {
+  children: ReactNode;
+}
+
+interface UserData {
+  email: string;
+  nickName: string;
+  isSeller: boolean;
+  uid: string;
+}
+
+const AuthContext = createContext({
+  user: null as UserData | null,
+});
+
+export const AuthContextProvider = ({ children }: AuthProps) => {
+  const auth = getAuth(app);
+  const [currentUser, setCurrentUser] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        //사용자의 기본 정보 가져오기
+
+        //Firestore에서 추가 정보 가져오기
+        try {
+          const userData = await getUserDataFromFirestore(user.uid);
+
+          if (userData) {
+            console.log("유저데이터", userData);
+            setCurrentUser({
+              email: userData.email,
+              nickName: userData.nickName,
+              isSeller: userData.isSeller,
+              uid: userData.uid,
+            });
+          }
+        } catch (error) {
+          console.error("에러", error);
+        }
+      } else {
+        setCurrentUser(user);
+      }
+    });
+  }, [auth]);
+
+
+  return (
+    <AuthContext.Provider value={{ user: currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
+
