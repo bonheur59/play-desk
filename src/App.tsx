f98ded943cbbfbec46ec417f2f1f9914b4a658a1
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2024-01-31 09:07:40 +0900

    refactor : onAuthChanged를 AuthContext로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "./firebaseApp";
-import { getUserDataFromFirestore } from "./firebaseUtil";
+// import { getUserDataFromFirestore } from "./firebaseUtil";
 
 function App() {
   const auth = getAuth(app);
@@ -14,27 +14,11 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
-  const [isSeller, setIsSeller] = useState<boolean | null>(null); // isSeller 상태 추가
 
-  //auth값이 바뀔때마다 AuthState가 실시간으로 반영됨
   useEffect(() => {
-    onAuthStateChanged(auth, async (user) => {
+    onAuthStateChanged(auth, (user) => {
       if (user) {
-        console.log(user);
         setIsAuthenticated(true);
-        try {
-          // 사용자가 인증된 경우에만 Firestore에서 사용자 데이터를 가져와 isSeller 값을 설정
-          const userData = await getUserDataFromFirestore(user.uid);
-          console.log("유저데이터", userData);
-          if (userData) {
-            setIsSeller(userData.isSeller);
-          }
-        } catch (error) {
-          console.error(
-            "Firestore에서 사용자 데이터를 가져오는 중 오류 발생:",
-            error
-          );
-        }
       } else {
         setIsAuthenticated(false);
       }
@@ -42,15 +26,7 @@ function App() {
     });
   }, [auth]);
 
-  return (
-    <>
-      {init ? (
-        <Router isAuthenticated={isAuthenticated} isSeller={isSeller} />
-      ) : (
-        "loading"
-      )}
-    </>
-  );
+  return <>{init ? <Router isAuthenticated={isAuthenticated} /> : "loading"}</>;
 }
 
 export default App;
