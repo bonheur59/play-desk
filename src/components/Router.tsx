6fb22064174c6728a036a22ee00e58274840a3db
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2024-02-05 22:34:11 +0900

    add : 메인 레이아웃 추가

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,42 +2,52 @@ import HomePage from "@/pages/home";
 import SellList from "@/pages/seller/list";
 import LoginPage from "@/pages/users/login";
 import SignupPage from "@/pages/users/signup";
-import ProductCreate from "@/pages/seller/create";
+import ProductNew from "@/pages/seller/new";
 import MyPage from "@/pages/users/profile";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "@/context/AuthContext";
+import ProductEdit from "@/pages/seller/edit";
+import ProductDetail from "@/pages/shop/[id]";
+import ProductList from "@/pages/shop";
+import MainLayout from "@/layout/MainLayout";
 
 interface RouterProps {
   isAuthenticated: boolean;
-  isSeller: boolean | null;
 }
 
-const Router = ({ isAuthenticated, isSeller }: RouterProps) => {
+const Router = ({ isAuthenticated }: RouterProps) => {
+  const { user } = useContext(AuthContext);
+  console.log("라우터 user", user);
+  console.log("라우터 isAuth", isAuthenticated);
   return (
     <Routes>
       {isAuthenticated ? (
         <>
-          {/* isSeller 값에 따라 판매자 페이지 또는 홈 페이지로 이동 */}
-          {isSeller !== null ? (
-            isSeller ? (
+          <Route element={<MainLayout />}>
+            {/* 판매자 */}
+            {user?.isSeller ? (
               <>
+                {" "}
                 <Route path="/seller/list" element={<SellList />} />
-                <Route path="/seller/create" element={<ProductCreate />} />
-                <Route path="/users/profile" element={<MyPage />} />
+                <Route path="/seller/new" element={<ProductNew />} />
               </>
             ) : (
-              <>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/users/profile" element={<MyPage />} />
-              </>
-            )
-          ) : (
-            <>
-              <Route path="/users/profile" element={<MyPage />} />
-            </>
-          )}
+              <> </>
+            )}
+            {/* 로그인된 모든 회원 */}
+
+            <Route path="/" element={<HomePage />} />
+            <Route path="/users/profile" element={<MyPage />} />
+            <Route path="/seller/edit/:id" element={<ProductEdit />} />
+          </Route>
         </>
       ) : (
         <>
+          <Route element={<MainLayout />}>
+            <Route path="/shop" element={<ProductList />} />
+            <Route path="/shop/:id" element={<ProductDetail />} />
+          </Route>
           <Route path="/users/login" element={<LoginPage />} />
           <Route path="/users/signup" element={<SignupPage />} />
           <Route path="*" element={<Navigate replace to="/" />} />
