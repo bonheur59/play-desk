2cfcbab9fbac5835563f2c6ea0105ace506fd67b
yhL <165865490+bonheur59@users.noreply.github.com>
2024-04-05 22:21:15 +0900

    refactor : 서버와의 통신 로직 분리

--- src/components/users/LoginForm.tsx
+++ src/components/users/LoginForm.tsx
@@ -1,115 +1,73 @@
-import * as z from "zod";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import React from "react";
+import Button from "../common/Button";
+import { Input } from "../ui/input";
+import { FieldErrors, SubmitHandler, UseFormReturn } from "react-hook-form";
+
+interface LoginFormProps {
+  type: "buyer" | "seller";
+  onSubmit: SubmitHandler<FormData>;
+  handleSubmit: UseFormReturn<FormData>["handleSubmit"];
+  register: UseFormReturn<FormData>["register"];
+  errors: FieldErrors<FormData>;
+}
 
 interface FormData {
   email: string;
   password: string;
+  shopName?: string; // seller 유형에만 존재
 }
 
-interface LoginFormProps {
-  onSubmit: SubmitHandler<FormData>;
-}
-
-const formSchema = z.object({
-  email: z.string().email({ message: "올바른 이메일 주소를 입력하세요." }),
-  password: z.string().min(8, { message: "비밀번호를 8자 이상 입려하세요" }),
-});
-
-const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
-  const form = useForm<FormData>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const handleSubmit = (values: z.infer<typeof formSchema>) => {
-    onSubmit(values);
-  };
-
+const LoginForm: React.FC<LoginFormProps> = ({
+  type,
+  onSubmit,
+  handleSubmit,
+  register,
+  errors,
+}) => {
   return (
-    <>
-      <main className="flex items-center justify-center h-screen bg-gray-100 ">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(handleSubmit)}
-            className="flex flex-col w-96 p-6 shadow-sm items-center justify-center gap-4 bg-white rounded-md"
-          >
-            <div>
-              이미지 자리 입니다.
-              <img />
-            </div>
-            {/* <div className="space-beetween">
-              <button>구매자</button>
-              <button>판매자</button>
-            </div> */}
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => {
-                return (
-                  <FormItem className="w-full ">
-                    <FormLabel>이메일</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="email"
-                        className="bg-gray-50 text-gray-500 outline-none"
-                        // placeholder="이메일을 입력하세요"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => {
-                return (
-                  <FormItem className="w-full ">
-                    <FormLabel>비밀번호</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        className="bg-gray-50 text-gray-500 outline-none"
-                        placeholder="비밀번호를 입력하세요"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-
-            <Button type="submit" className="w-full mt-5 bg-main-color">
-              로그인
-            </Button>
-            <div className="text-sm">
-              <span className="mr-3 text-gray-400">회원이 아니신가요?</span>
-              <Link to="/users/signup" className="underline">
-                회원가입
-              </Link>
-            </div>
-          </form>
-        </Form>
-      </main>
-    </>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col w-full gap-4"
+    >
+      <div>
+        <label className="text-sm font-medium">이메일</label>
+        <Input
+          type="text"
+          className="bg-gray-50 text-gray-500 outline-none"
+          {...register("email")}
+        />
+        {errors.email && (
+          <p className="text-red-500 text-sm">{errors.email?.message}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-sm font-medium">비밀번호</label>
+        <Input
+          type="password"
+          className="bg-gray-50 text-gray-500 outline-none"
+          {...register("password")}
+        />
+        {errors.password && (
+          <p className="text-red-500 text-sm">{errors.password?.message}</p>
+        )}
+      </div>
+      {type === "seller" && (
+        <div>
+          <label className="text-sm font-medium">상점 이름</label>
+          <Input
+            type="text"
+            className="bg-gray-50 text-gray-500 outline-none"
+            {...register("shopName")}
+          />
+          {errors.shopName && (
+            <p className="text-red-500 text-sm">{errors.shopName?.message}</p>
+          )}
+        </div>
+      )}
+      <Button type="submit" size="w-full" color="main" font="medium">
+        버튼
+      </Button>
+    </form>
   );
 };
 
--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,40 +1,145 @@
-import { useNavigate } from "react-router-dom";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { FirebaseError } from "firebase/app";
-import { app, db } from "@/firebaseApp";
-
 import LoginForm from "@/components/users/LoginForm";
+import UserLayout from "@/layout/UserLayout";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-interface FormData {
+type FormData = {
   email: string;
   password: string;
-}
+  shopName?: string;
+};
+
+const schema = (userType: string) => {
+  if (userType === "seller") {
+    return z.object({
+      email: z.string().min(1, { message: "이메일을 입력해주세요" }),
+      password: z
+        .string()
+        .trim()
+        .regex(
+          /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/,
+          {
+            message: `비밀번호는 8자 이상의 영문 대소문자와 숫자, 특수문자를 포함하여야 합니다.`,
+          }
+        ),
+      shopName: z.string().min(1, { message: "상점이름을 입력해주세요" }),
+    });
+  } else {
+    return z.object({
+      email: z.string().min(1, { message: "이메일을 입력해주세요" }),
+      password: z
+        .string()
+        .trim()
+        .regex(
+          /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/,
+          {
+            message: `비밀번호는 8자 이상의 영문 대소문자와 숫자, 특수문자를 포함하여야 합니다.`,
+          }
+        ),
+    });
+  }
+};
 
 const LoginPage = () => {
-  const navigate = useNavigate();
+  const [userType, setUserType] = useState<"buyer" | "seller">("buyer");
 
-  const onSubmit = async (formData: FormData) => {
-    console.log("전달받은 form data", formData);
-    const { email, password } = formData;
-
-    try {
-      const auth = getAuth(app);
-      await signInWithEmailAndPassword(auth, email, password);
-      alert("로그인에 성공했습니다.");
-      navigate("/seller/list");
-    } catch (error) {
-      if (error instanceof FirebaseError) {
-        // FirebaseError에 대한 처리
-        console.error("Firebase 에러 코드:", error.code);
-        console.error("Firebase 에러 메시지:", error.message);
-      } else {
-        // 다른 타입의 에러에 대한 처리
-        console.error("일반 에러:", error);
-      }
-    }
+  const handleUserType = (userType: "buyer" | "seller") => {
+    setUserType(userType);
   };
 
-  return <LoginForm onSubmit={onSubmit} />;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema(userType)),
+    defaultValues: {
+      email: "",
+      password: "",
+      shopName: userType === "seller" ? "" : undefined, // 판매자일 경우에만 shopName을 초기화
+    },
+  });
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
+  return (
+    <UserLayout>
+      <div className="flex gap-10">
+        <button
+          className={`p-2 m-2 w-[70px] h-10 ${
+            userType === "buyer" ? "text-main-color" : ""
+          }`}
+          onClick={() => handleUserType("buyer")}
+        >
+          buyer
+        </button>
+        <button
+          className={`p-2 m-2 w-[70px] h-10 ${
+            userType === "seller" ? "text-main-color" : ""
+          }`}
+          onClick={() => handleUserType("seller")}
+        >
+          seller
+        </button>
+      </div>
+      <LoginForm
+        type={userType}
+        handleSubmit={handleSubmit}
+        onSubmit={onSubmit}
+        errors={errors}
+        register={register}
+      />
+    </UserLayout>
+  );
 };
 
 export default LoginPage;
+
+// import { useNavigate } from "react-router-dom";
+// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+// import { FirebaseError } from "firebase/app";
+// import { app, db } from "@/firebaseApp";
+
+// import LoginForm from "@/components/users/LoginForm";
+
+// interface FormData {
+//   email: string;
+//   password: string;
+// }
+
+// const LoginPage = () => {
+//   const navigate = useNavigate();
+
+//   const onSubmit = async (formData: FormData) => {
+//     console.log("전달받은 form data", formData);
+//     const { email, password } = formData;
+
+//     try {
+//       const auth = getAuth(app);
+//       await signInWithEmailAndPassword(auth, email, password);
+//       alert("로그인에 성공했습니다.");
+//       navigate("/seller/list");
+//     } catch (error) {
+//       if (error instanceof FirebaseError) {
+//         // FirebaseError에 대한 처리
+//         console.error("Firebase 에러 코드:", error.code);
+//         console.error("Firebase 에러 메시지:", error.message);
+//       } else {
+//         // 다른 타입의 에러에 대한 처리
+//         console.error("일반 에러:", error);
+//       }
+//     }
+//   };
+
+//   // return <LoginForm onSubmit={onSubmit} />;
+//   return (
+//     <>
+//       <div> 로그인페이지 입니다. </div>
+//     </>
+//   );
+// };
+
+// export default LoginPage;
