aae656a272f9f3fe6ea67c04170b3885f8d9809a
yhL <165865490+bonheur59@users.noreply.github.com>
2024-04-08 21:30:50 +0900

    chore : user 정보 Authcontext에서 불러오도록 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -29,32 +29,38 @@ interface RouterProps {
   user: userType | null;
 }
 
-const Router = ({ user }: RouterProps) => {
-  if (user === null) return null;
+const Router = () => {
+  // console.log("라우터 유저", user);
+  const { user } = useContext(AuthContext);
 
   return (
     <Routes>
       {/* 로그인 & 비로그인 공통 */}
-      <Route path="/" element={<ProductMainPage />} />
-      <Route path="/shop/product/:id" element={<ProductDetail />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<ProductMainPage />} />
+        <Route path="/shop/product/:id" element={<ProductDetail />} />
+      </Route>
       {/* 로그인 되어있지 않아야 할 경우 */}
-      <Route element={<PrivateRoute user={user} />}>
+      <Route element={<PrivateRoute condition={!user} />}>
         <Route path="/users/login" element={<LoginPage />} />
         <Route path="/users/signup" element={<SignupPage />} />
       </Route>
 
       {/* 로그인이 필요한 경우 */}
-      <Route element={<PrivateRoute user={user} />}>
+
+      <Route element={<PrivateRoute condition={user ? true : false} />}>
         <Route path="/users/mypage" element={<MyPage />} />
         <Route path="/cart" />
         <Route path="/order" element={<OrderListPage />} />
         <Route path="/users/myorder" />
         {/* 판매자 접근 */}
-        <Route element={<SellerRoute seller={user.isSeller} />}>
-          <Route path="/seller/list" />
-          <Route path="/seller/new" />
-          <Route path="/seller/edit/:id" />
-        </Route>
+        {user && (
+          <Route element={<SellerRoute seller={user.isSeller} />}>
+            <Route path="/seller/list" element={<SellList />} />
+            <Route path="/seller/new" />
+            <Route path="/seller/edit/:id" />
+          </Route>
+        )}
       </Route>
     </Routes>
   );
