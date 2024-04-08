4f2c722f43d9611c86fe96fce016899059861810
yhL <165865490+bonheur59@users.noreply.github.com>
2024-04-08 21:26:24 +0900

    fix : 회원가입시 user 정보 업데이트 버그 수정
    회원가입 시 user 정보가 바로 업데이트 되지 않고 새로고침 해야 업데이트 되는 버그 수정

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,9 @@
 import { ReactNode, createContext, useState, useEffect } from "react";
-import { User, getAuth, onAuthStateChanged } from "firebase/auth";
+import { User, getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { getUserDataFromFirestore } from "../firebaseUtil";
-import { app } from "@/firebaseApp";
+import { app, db } from "@/firebaseApp";
+import { doc, onSnapshot } from "firebase/firestore";
+import { Auth } from "firebase/auth/cordova";
 
 interface AuthProps {
   children: ReactNode;
@@ -14,40 +16,68 @@ interface UserData {
   uid: string;
 }
 
-const AuthContext = createContext({
+interface AuthContextType {
+  user: UserData | null;
+  logout: () => Promise<void>; // logout 함수의 타입 추가
+}
+
+const AuthContext = createContext<AuthContextType>({
   user: null as UserData | null,
+  logout: async () => {},
 });
 
 export const AuthContextProvider = ({ children }: AuthProps) => {
   const auth = getAuth(app);
   const [currentUser, setCurrentUser] = useState<UserData | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const logout = async () => {
+    await signOut(auth);
+    setCurrentUser(null);
+  };
 
   useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
         try {
-          const userData = await getUserDataFromFirestore(user.uid);
+          const docRef = doc(db, "User", user.uid);
+          onSnapshot(docRef, (doc) => {
+            if (doc.exists()) {
+              const data = doc.data();
+              const user = {
+                email: data.email,
+                nickName: data.nickName,
+                isSeller: data.isSeller,
+                uid: data.uid,
+              };
+              setCurrentUser(user);
+            }
+          });
+          // const userData = await getUserDataFromFirestore(user.uid);
+          // console.log("유저데이터", userData);
 
-          if (userData) {
-            console.log("유저데이터", userData);
-            setCurrentUser({
-              email: userData.email,
-              nickName: userData.nickName,
-              isSeller: userData.isSeller,
-              uid: userData.uid,
-            });
-          }
+          // if (userData) {
+          //   setCurrentUser({
+          //     email: userData.email,
+          //     nickName: userData.nickName,
+          //     isSeller: userData.isSeller,
+          //     uid: userData.uid,
+          //   });
+          // }
         } catch (error) {
           console.error("에러", error);
         }
       } else {
-        setCurrentUser(user);
+        setLoading(false);
       }
     });
   }, [auth]);
+  console.log("커런트 유저", currentUser);
+
+
 
   return (
-    <AuthContext.Provider value={{ user: currentUser }}>
+    <AuthContext.Provider value={{ user: currentUser, logout }}>
       {children}
     </AuthContext.Provider>
   );
