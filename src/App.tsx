fb39f549623ae6a284e25a1524c9a64d442926ac
yhL <165865490+bonheur59@users.noreply.github.com>
2024-04-09 14:36:14 +0900

    chore : 사용하지 않는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,7 @@
-// import { useContext, useEffect, useState } from "react";
-import { useContext } from "react";
 import Router from "./routes/Router";
-import AuthContext from "./context/AuthContext";
-// import "./App.css";
-
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
-// import { app } from "./firebaseApp";
-
-// import AuthContext from "./context/AuthContext";
-// import { useNavigate } from "react-router-dom";
 
 function App() {
-  return <>{<Router />}</>;
-  // return (
-  //   <>
-  //     <div> 안녕</div>
-  //   </>
-  // );
-  // const auth = getAuth(app);
-  // const { user } = useContext(AuthContext);
-  // const navigate = useNavigate();
-
-  //auth를 체크하기 전(initallize 전)에는 loader를 띄워주는 용도
-  // const [init, setInit] = useState<boolean>(false);
-  // //auth의 CurrentUser가 있으면 authenticated로 변경
-  // const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
-  //   !!auth?.currentUser
-  // );
-
-  // console.log(user?.isSeller)
-
-  // useEffect(() => {
-  //   onAuthStateChanged(auth, (userState) => {
-  //     if (userState) {
-  //       setIsAuthenticated(true);
-  //       if (user?.isSeller) {
-  //         navigate(`/seller/list`);
-  //       } else {
-  //         navigate(`/shop`);
-  //       }
-  //     } else {
-  //       setIsAuthenticated(false);
-  //     }
-  //     setInit(true);
-  //   });
-  // }, [auth]);
-
-  // return (
-  //   <>
-  //     <Router />
-  //   </>
-  // );
+  return <Router />;
 }
 
 export default App;
