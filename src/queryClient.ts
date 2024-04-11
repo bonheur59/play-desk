d1d2fe45cd08a7fbae7affe5b34427a0c9025d1f
yhL <165865490+bonheur59@users.noreply.github.com>
2024-04-11 18:28:55 +0900

    chore : setDefaultOptions 비활성화

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -1,11 +1,4 @@
-import { log } from "console";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
+import { QueryClient } from "react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,14 +7,12 @@ const queryClient = new QueryClient({
       staleTime: 1000 * 60,
       refetchOnWindowFocus: false,
       refetchOnMount: false,
-      suspense: false, // Suspense 비활성화
+      suspense: true,
     },
   },
 });
 
 //특정 쿼리에 대한 재정의
-queryClient.setDefaultOptions({});
+// queryClient.setDefaultOptions({});
 
 export default queryClient;
-
-
