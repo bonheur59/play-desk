ccf49999ac4b589381d229b481bf49005ac4897c
yhL <165865490+bonheur59@users.noreply.github.com>
2024-04-11 23:24:29 +0900

    feat : 임시 suspense 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -33,47 +33,48 @@ interface RouterProps {
 
 const Router = () => {
   const { user, loading } = useContext(AuthContext);
-  console.log("유저 정보", user?.isSeller);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <Routes>
-      {/* 로그인 & 비로그인 공통 */}
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<ProductMainPage />} />
-        <Route path="/shop/product/:id" element={<ProductDetail />} />
-      </Route>
-      {/* 로그인 되어있지 않아야 할 경우 */}
-      <Route element={<PrivateRoute condition={!user} />}>
-        <Route path="/users/login" element={<LoginPage />} />
-        <Route path="/users/signup" element={<SignupPage />} />
-      </Route>
-
-      {/* 로그인이 필요한 경우 */}
-      <Route element={<MainLayout />}>
-        <Route element={<PrivateRoute condition={user ? true : false} />}>
-          <Route path="/cart" />
-          <Route path="/order" element={<OrderListPage />} />
-          <Route element={<MypageLayout />}>
-            <Route path="/users/myorder" element={<OrderPage />} />
-            <Route path="/users/profile" element={<MyPage />} />
-          </Route>
+    <Suspense fallback="..loading">
+      <Routes>
+        {/* 로그인 & 비로그인 공통 */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<ProductMainPage />} />
+          <Route path="/shop/product/:id" element={<ProductDetail />} />
         </Route>
-        {/* 판매자 접근 */}
-        {user && (
-          <Route element={<SellerRoute seller={user.isSeller} />}>
-            <Route path="/seller/list" element={<SellList />} />
-            <Route path="/seller/new" />
-            <Route path="/seller/edit/:id" />
+        {/* 로그인 되어있지 않아야 할 경우 */}
+        <Route element={<PrivateRoute condition={!user} />}>
+          <Route path="/users/login" element={<LoginPage />} />
+          <Route path="/users/signup" element={<SignupPage />} />
+        </Route>
+
+        {/* 로그인이 필요한 경우 */}
+        <Route element={<MainLayout />}>
+          <Route element={<PrivateRoute condition={user ? true : false} />}>
+            <Route path="/cart" />
+            <Route path="/order" element={<OrderListPage />} />
+            <Route element={<MypageLayout />}>
+              <Route path="/users/myorder" element={<OrderPage />} />
+              <Route path="/users/profile" element={<MyPage />} />
+            </Route>
           </Route>
-        )}
-      </Route>
-      {/* 이외의 경로 처리 */}
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+          {/* 판매자 접근 */}
+          {user && (
+            <Route element={<SellerRoute seller={user.isSeller} />}>
+              <Route path="/seller/list" element={<SellList />} />
+              <Route path="/seller/new" element={<ProductNew />} />
+              <Route path="/seller/edit/:id" element={<ProductEdit />} />
+            </Route>
+          )}
+        </Route>
+        {/* 이외의 경로 처리 */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Suspense>
   );
 };
 
